6b834fdd4c99b4021a82f0d0341dd7e715b22790
pamiyummy <hakw4morta@gmail.com>
2022-04-26 18:34:56 +0900

    ノードをクリックするとエラーが出る。そもそもクラスがうまく機能していない？

--- devenv/track/static/track/selected_nodes.js
+++ devenv/track/static/track/selected_nodes.js
@@ -1,21 +1,55 @@
-const nodes = [];
 
-const SelectedNodes = {
+// export はdjangoでは厳しそうな感じだった。
+
+// const nodes = [];
+
+// const SelectedNodes = {
     
-    push(node, color) {
-        nodes.push( { 'node': node, 'id': node.id() } );
+//     push(node, color) {
+//         nodes.push( { 'node': node, 'id': node.id() } );
+//         node.style('background-color', color);
+//     },
+
+//     pop() {
+//         nodes.pop().removeStyle();
+//     },
+
+//     clear() {
+//         while(nodes.length !== 0) {
+//             nodes.pop().removeStyle();
+//         }
+//     }
+// };
+
+// Object.freeze(SelectedNodes);
+
+class SelectedNodes {
+    #nodes = [];
+
+    constructor() {}
+
+    push_and_color(node, color) {
+        // TODO: nodeがnode型じゃなかったらエラー
+        this.#nodes.push( { 'node': node, 'id': node.id() } );
         node.style('background-color', color);
-    },
+        // console.log(this.#nodes);
+    }
 
-    pop() {
-        nodes.pop().removeStyle();
-    },
-
-    clear() {
-        while(nodes.length !== 0) {
-            nodes.pop().removeStyle();
+    pop_and_decolor() {
+        // TODO: popしたものがnullだったらエラー？エラー処理必要ないかも？
+        if(this.#nodes.length > 0) {
+            // console.log(this.#nodes);
+            this.#nodes.pop().removeStyle();
         }
     }
-};
 
-Object.freeze(SelectedNodes);
+    clear_and_decolor() {
+        while(this.#nodes.length !== 0) {
+            this.#nodes.pop().removeStyle();
+        }
+    }
+
+    get get_nodes() {
+        return this.#nodes;
+    }
+}
