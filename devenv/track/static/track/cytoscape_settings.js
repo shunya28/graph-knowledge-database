d7048d93b148f7be9ae6b1d97df9a1a98e153a75
pamiyummy <hakw4morta@gmail.com>
2022-05-10 13:56:07 +0900

    エラーの修正完了とコードの整理

--- devenv/track/static/track/cytoscape_settings.js
+++ devenv/track/static/track/cytoscape_settings.js
@@ -1,44 +1,44 @@
-function show_create_page() {
-    $('#article-detail').hide();
-    $('#article-delete').hide();
-    $('#article-create').show();
+// function show_create_page() {
+//     $('#article-detail').hide();
+//     $('#article-delete').hide();
+//     $('#article-create').show();
 
-    if(colored_nodes.length !== 0) {
-        colored_nodes.shift().removeStyle();
-    }
-}
+//     if(colored_nodes.length !== 0) {
+//         colored_nodes.shift().removeStyle();
+//     }
+// }
 
-function show_delete_page() {
-    $('#article-detail').hide();
-    $('#article-create').hide();
-    $('#article-delete').show();
+// function show_delete_page() {
+//     $('#article-detail').hide();
+//     $('#article-create').hide();
+//     $('#article-delete').show();
 
-    if(colored_nodes.length !== 0) {
-        colored_nodes.shift().removeStyle();
-    }
-}
+//     if(colored_nodes.length !== 0) {
+//         colored_nodes.shift().removeStyle();
+//     }
+// }
 
-function hide_article_page() {
-    // 一つ前にクリックされたノードの色をデフォルトに戻す
-    if(colored_nodes !== 0) {
-        colored_nodes.shift().removeStyle();
-    }
-    $('#article-detail').hide();
-}
+// function hide_article_page() {
+//     // 一つ前にクリックされたノードの色をデフォルトに戻す
+//     if(colored_nodes !== 0) {
+//         colored_nodes.shift().removeStyle();
+//     }
+//     $('#article-detail').hide();
+// }
 
-function hide_create_page() {
-    reference_list.length = 0;
-    $('#references').text(reference_list);
+// function hide_create_page() {
+//     reference_list.length = 0;
+//     $('#references').text(reference_list);
 
-    // 色付けされたノードを全て元の色に戻す
-    while(colored_nodes.length !== 0) {
-        colored_nodes.shift().removeStyle();
-    }
+//     // 色付けされたノードを全て元の色に戻す
+//     while(colored_nodes.length !== 0) {
+//         colored_nodes.shift().removeStyle();
+//     }
 
-    $('#article-create').hide();
-}
+//     $('#article-create').hide();
+// }
 
-function hide_delete_page() {
+// function hide_delete_page() {
 
-    $('#article-delete').hide();
-}
+//     $('#article-delete').hide();
+// }
