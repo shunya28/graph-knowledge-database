bd1795b3c4e3547d089cf2e4d0d3789ddf4819fb
pamiyummy <hakw4morta@gmail.com>
2022-06-14 16:27:57 +0900

    アカウント登録機能の追加

--- devenv/accounts/forms.py
+++ devenv/accounts/forms.py
@@ -1,4 +1,19 @@
-from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class UserCreateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # htmlの表示を変更可能にします
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2",)
 
 
 class LoginForm(AuthenticationForm):
--- devenv/accounts/views.py
+++ devenv/accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin  # ログインしたユーザだけ閲覧できるように制限するクラス
-from django.contrib.auth.views import(LoginView, LogoutView)  # ログイン、ログアウト機能
-from .forms import LoginForm
+from django.contrib.auth.views import LoginView, LogoutView  # ログイン、ログアウト機能
+from .forms import LoginForm, UserCreateForm
+from django.views.generic import CreateView
+from django.contrib.auth import login, authenticate
 
 
 class Login(LoginView):
@@ -13,3 +15,28 @@ class Login(LoginView):
 class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'track/index.html'
+
+
+class Signup(CreateView):
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            # フォームから'username'を読み取る
+            username = form.cleaned_data.get('username')
+
+            # フォームから'password1'を読み取る
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'accounts/signup.html', {'form': form, })
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        return render(request, 'accounts/signup.html', {'form': form, })
+
+
+# create_account = Create_account.as_view()
