b40ffdb760e50abf425073d9563ffacbc91be050
shunya28 <hakw4morta@gmail.com>
2022-07-15 17:51:49 +0900

    記事の著者が今のユーザであるときに、編集・削除ボタンが出るようにした

--- devenv/track/static/track/cytoscape.js
+++ devenv/track/static/track/cytoscape.js
@@ -78,12 +78,34 @@ cy.on('tap', 'node', function(evt) {
         for(let i = 0; i < graph_data.length; i++) {
             if(node.id() === graph_data[i].data.id) {
                 document.getElementById('article-title').textContent = graph_data[i].data.title;
-                document.getElementById('author').textContent = graph_data[i].data.author;
+                const author = document.getElementById('author').textContent = graph_data[i].data.author;
                 document.getElementById('creation-date').textContent = graph_data[i].data.creation_date;
                 document.getElementById('article-body').innerHTML = markdown_to_sanitized_html(graph_data[i].data.body);
                 document.querySelectorAll('pre, code').forEach((el) => {
                     hljs.highlightElement(el);
                 });
+
+                // 今のユーザがこの記事の著者なら、記事の編集ボタンと削除ボタンを表示
+                const txt_closebtn = `
+                    <div class="btn-right">
+                        <a href="#" class="btn-page" onclick="hide_article_page()"><i class="fa-solid fa-xmark"></i></a>
+                    </div>
+                `;
+                const txt_edit_permitted = `
+                    <div class="btn-left">
+                        <a href="#" class="btn-page"><i class="fa-solid fa-pen"></i></a>
+                    </div>
+                    <div class="btn-left">
+                        <a href="#" class="btn-page"><i class="fa-solid fa-trash"></i></a>
+                    </div>
+                `;               
+
+                if(author === current_user) {
+                    document.getElementById('btn-for-detail').innerHTML = txt_closebtn + txt_edit_permitted;
+                } else {
+                    document.getElementById('btn-for-detail').innerHTML = txt_closebtn;
+                }
+
                 break;
             }
         }
